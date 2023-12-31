ed18944cfcf4453d00727e32f8d0a4d7ad481192
Milad Remote <mniroee@gmail.com>
2023-12-31 21:33:20 +0330

    make Everything better and Clean :))

--- dist/price.js
+++ dist/price.js
@@ -1,10 +1,39 @@
+import { ExpectedData } from "./ExpectedData.js";
 document.addEventListener("DOMContentLoaded", function () {
+    var currencyData = [];
+    fetch("https://call1.tgju.org/ajax.json")
+        .then(function (res) { return res.json(); })
+        .then(function (data) {
+        if (data.hasOwnProperty("current")) {
+            var currentData_1 = data.current;
+            ExpectedData.forEach(function (data) {
+                if (currentData_1.hasOwnProperty(data.name)) {
+                    currencyData.push({
+                        title: data.title,
+                        price: currentData_1[data.name].p,
+                        status: currentData_1[data.name].dt,
+                    });
+                }
+            });
+            if (currencyData.length === 7) {
+                PrintValues(currencyData);
+            }
+        }
+        else {
+            console.error("Error in getting currency data");
+        }
+    });
+});
+function PrintValues(Currencies) {
     var currencyContainer = document.createElement("div");
     currencyContainer.classList.add("currency__container");
     document.body.appendChild(currencyContainer);
     var lastUpdateContainer = document.createElement("div");
     lastUpdateContainer.classList.add("currency__last-update");
     currencyContainer.appendChild(lastUpdateContainer);
+    var currencySubContainer = document.createElement('div');
+    currencySubContainer.classList.add("currency__sub-container");
+    currencyContainer.appendChild(currencySubContainer);
     function updateLastUpdateTime() {
         lastUpdateContainer.innerHTML = "\u0622\u062E\u0631\u06CC\u0646 \u0628\u0631\u0648\u0632\u0631\u0633\u0627\u0646\u06CC: ".concat(new Date()
             .toLocaleTimeString("en-US", { hour12: false })
@@ -14,93 +43,31 @@ document.addEventListener("DOMContentLoaded", function () {
     setInterval(updateLastUpdateTime, 1000 * 60);
     var currencyPrices = document.createElement("div");
     currencyPrices.classList.add("currency__prices");
-    currencyContainer.appendChild(currencyPrices);
-    var currencyData = [];
-    fetch("https://call1.tgju.org/ajax.json")
-        .then(function (res) { return res.json(); })
-        .then(function (data) {
-        if (data.hasOwnProperty("current")) {
-            var currentData = data.current;
-            if (currentData.hasOwnProperty("sekeb")) {
-                currencyData.push({
-                    title: "سکه بهار آزادی",
-                    price: currentData.sekeb.p,
-                    status: currentData.sekeb.dt,
-                });
+    currencySubContainer.appendChild(currencyPrices);
+    currencyPrices.innerHTML = "";
+    Currencies.forEach(function (item) {
+        var div1 = document.createElement("div");
+        div1.classList.add("currency__item");
+        {
+            var div2 = document.createElement("div");
+            div2.classList.add("currency__item-title");
+            div2.classList.add(item.status.length === 0 ? 'const' : item.status);
+            div2.innerText = item.title;
+            div1.appendChild(div2);
+            var div3 = document.createElement("div");
+            div3.classList.add("currency__item-price");
+            div3.innerText = item.price;
+            {
+                var span = document.createElement("span");
+                span.style.color = "#999";
+                span.style.fontSize = "10px";
+                span.innerText = " ریال";
+                div3.appendChild(span);
             }
-            if (currentData.hasOwnProperty("sekee")) {
-                currencyData.push({
-                    title: "سکه امامی",
-                    price: currentData.sekee.p,
-                    status: currentData.sekee.dt,
-                });
-            }
-            if (currentData.hasOwnProperty("nim")) {
-                currencyData.push({
-                    title: "نیم سکه",
-                    price: currentData.nim.p,
-                    status: currentData.nim.dt,
-                });
-            }
-            if (currentData.hasOwnProperty("rob")) {
-                currencyData.push({
-                    title: "ربع سکه",
-                    price: currentData.rob.p,
-                    status: currentData.rob.dt,
-                });
-            }
-            if (currentData.hasOwnProperty("price_dollar_rl")) {
-                currencyData.push({
-                    title: "دلار آمریکا",
-                    price: currentData.price_dollar_rl.p,
-                    status: currentData.price_dollar_rl.dt,
-                });
-            }
-            if (currentData.hasOwnProperty("price_eur")) {
-                currencyData.push({
-                    title: "یورو اروپا",
-                    price: currentData.price_eur.p,
-                    status: currentData.price_eur.dt,
-                });
-            }
-            if (currentData.hasOwnProperty("price_try")) {
-                currencyData.push({
-                    title: "لیر ترکیه",
-                    price: currentData.price_try.p,
-                    status: currentData.price_try.dt,
-                });
-            }
-            if (currencyData.length === 7) {
-                currencyPrices.innerHTML = "";
-                currencyData.forEach(function (item) {
-                    var div1 = document.createElement("div");
-                    div1.classList.add("currency__item");
-                    {
-                        var div2 = document.createElement("div");
-                        div2.classList.add("currency__item-title");
-                        div2.classList.add(item.status);
-                        div2.innerText = item.title;
-                        div1.appendChild(div2);
-                        var div3 = document.createElement("div");
-                        div3.classList.add("currency__item-price");
-                        div3.innerText = item.price;
-                        {
-                            var span = document.createElement("span");
-                            span.style.color = "#999";
-                            span.innerText = "ریال";
-                            div3.appendChild(span);
-                        }
-                        div1.appendChild(div3);
-                    }
-                    currencyPrices.appendChild(div1);
-                });
-                currencyContainer.style.display = "block";
-                document.body.style.marginBottom =
-                    currencyContainer.offsetHeight + "px";
-            }
+            div1.appendChild(div3);
         }
-        else {
-            console.error("Error in getting currency data");
-        }
+        currencyPrices.appendChild(div1);
     });
-});
+    currencyContainer.style.display = "block";
+    document.body.style.marginBottom = currencyContainer.offsetHeight + "px";
+}
